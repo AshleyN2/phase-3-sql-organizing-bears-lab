a30d95294914774e804af1483411de9f198f4e11
Ashley <ashleynjeri7@gmail.com>
2022-11-03 19:27:33 +0300

    updated commit

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+
+VALUES
+('Mr. Chocolate', 15, 'M', 'brown', 'choleric', 1)
+('Rowdy', 8, 'F', 'beige', 'phlegmatic', 0 )
+('Tabitha',3, 'F', 'black', 'melancholic', 1)
+('Sergeant Brown', 9, 'M', 'white', 'sanguine', 0)
+('Melissa', 4, 'F', 'brown', 'choleric', 1)
+('Grinch', 5, 'M', 'beige', 'phlegmatic', 0)
+('Wendy', 1, 'F', 'black', 'melancholic', 1)
+(NULL, 11, 'M', 'white', 'sanguine', 0)
+
